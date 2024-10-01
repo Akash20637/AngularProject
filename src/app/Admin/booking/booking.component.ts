d79ce8b9ca2c21bb08496d78b92a3236d784dcc1
Akash <akashvarshney209@gmail.com>
2024-10-01 07:59:10 +0530

    Add Flight Data

--- src/app/Admin/booking/booking.component.ts
+++ src/app/Admin/booking/booking.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FlightService } from './booking.service';
+import { map } from 'rxjs';
+import { Flight } from './booking.model';
 
 @Component({
   selector: 'app-booking',
@@ -7,6 +10,17 @@ import { Component } from '@angular/core';
   templateUrl: './booking.component.html',
   styleUrl: './booking.component.css'
 })
-export class BookingComponent {
+export class BookingComponent implements OnInit{
+
+  flight_service = inject(FlightService)
+  flight_list : Flight[] = []
+
+  ngOnInit(): void {
+    this.flight_service.getAllFlights().subscribe({
+      next : (res) => this.flight_list = res,
+      error : (err)=> console.log(err),
+      complete : ()=> console.log("complete........")
+    })
+  }
 
 }
