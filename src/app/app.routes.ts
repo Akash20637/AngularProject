93d730b8731d5d5404cc58b3ab6de663e75790c4
Akash <akash.varshney@iktara.ai>
2024-10-08 13:42:23 +0530

    Add User Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,43 @@
 import { Routes } from "@angular/router";
 
+
 export const routes : Routes = [
     {
         path : '',
-        redirectTo : 'signup',
+        redirectTo : 'user',
         pathMatch : 'full',
         
     },
     {
-        path : 'signup',
-        loadComponent : ()=> import('./User/signup/signup.component').then(m=>m.SignupComponent),
-        title : 'SignUp'
+        path : 'user',
+        loadComponent : ()=> import('./User/user-layout/user-layout.component').then(m => m.UserLayoutComponent),
+        title : 'HomePage',
+        children : [
+            {
+                path : 'signup',
+                loadComponent : ()=> import('./User/signup/signup.component').then(m=>m.SignupComponent),
+                title : 'SignUp'
+            },
+            {
+                path : 'login',
+                loadComponent : ()=> import('./User/login/login.component').then(m=> m.LoginComponent),
+                title : 'Login'
+            },
+            {
+                path : 'book-flight',
+                loadComponent : ()=> import('./User/book-flight/book-flight.component').then(m=> m.BookFlightComponent),
+                title : 'BookFlight'
+            },
+            {
+                path : 'search-flight',
+                loadComponent : ()=> import('./User/search-flight/search-flight.component').then(m=> m.SearchFlightComponent),
+                title : 'SearchFlight'
+            },
+            {
+                path : 'bookings',
+                loadComponent : ()=> import('./User/check-bookings/check-bookings.component').then(m=> m.CheckBookingsComponent),
+                title : 'AllBookings'
+            },
+        ]
     }
 ]
